2548fde9bbaad063d1b690caff2f74f50c302d26
adjprtma <djitothebeat123@gmail.com>
2020-05-02 15:01:46 +0700

    update praktikum modul 7

--- praktikum/P7_PW_193040101/latihan7c/php/detail.php
+++ praktikum/P7_PW_193040101/latihan7c/php/detail.php
@@ -23,10 +23,10 @@
 <head>
     <title>Detail</title>
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="../js/materialize.min.js"></script>
 
     <!-- my CSS -->
     <link rel="stylesheet" href="../css/style.css">
--- praktikum/P7_PW_193040101/latihan7c/php/login.php
+++ praktikum/P7_PW_193040101/latihan7c/php/login.php
@@ -60,10 +60,10 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="../js/materialize.min.js"></script>
 
     <!-- my CSS -->
     <link rel="stylesheet" href="../css/style.css">
